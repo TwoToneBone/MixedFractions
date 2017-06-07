ee8abf75e85e6d9b851aa688e8f41260ff97a2eb
Erik <erik.ra@gmail.com>
2017-06-07 17:13:39 +0200

    Add project files.

---
+++ MixedFractions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixedFractions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> outputs = new List<string>();
+
+            while (true)
+            {
+                string[] inputs = Console.ReadLine().Split(' ');
+                int numerator = int.Parse(inputs[0]);
+                int denominator = int.Parse(inputs[1]);
+
+                if (numerator == 0 && denominator == 0)
+                {
+                    break;
+                }               
+
+                outputs.Add($"{numerator/denominator} {numerator % denominator} / {denominator}");
+            }
+
+            foreach (var output in outputs)
+            {
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
